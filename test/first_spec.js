d3ac65b6f01866013343ca8f081983fb0956fbd9
Diego Meneses <diegoxdc99@hotmail.com>
2017-11-16 10:37:53 -0500

    Creación de una prueba unitaria con coffeescript

---
+++ test/first_spec.js
@@ -0,0 +1,11 @@
+var assert = require('assert');
+
+describe('Una caracteristica', function(){
+    describe('Un escenario', function(){
+        it('hace algo esperado', function(){
+            var diego = 'diego';
+            console.log(diego);
+            assert(true);
+        })
+    })
+});
---
+++ test/second_spec.coffee
@@ -0,0 +1,6 @@
+assert = require "assert"
+
+describe "A second feacture", ->
+    describe "A scenario", ->
+        it "does something I want", ->
+            assert true
